aab4337d3530560ef99dd3973b70ffd98fbcf53a
JIAQI13 <jiaqi11@ualberta.ca>
2021-06-09 14:33:30 -0700

    add Empty & Headter to appointment

---
+++ src/components/Appointment/Index.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import classnames from "classnames";
+import "components/Appointment/styles.scss";
+import "components/Appointment/Header";
+import "components/Appointment/Empty";
+
+export default function Appointment(props) {
+    return (
+        <article className="appointment">asdfasd</article>
+    );
+}
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,9 @@ import DayList from "components/DayList";
 import DayListItem from "components/DayListItem";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/Index";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -131,3 +134,12 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
